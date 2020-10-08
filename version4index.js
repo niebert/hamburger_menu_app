d230dede59c67c4222b12d592e25341230e9d55b
Engelbert Niehaus <niehaus@uni-landau.de>
2020-10-08 16:40:50 +0200

    version4index.js added

---
+++ version4index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const pkg = require("./package.json");
+const b4c = require("build4code").codegen;
+// <div id="version">1.2.3.4</div>
+console.log("Set version for 'index.html' and 'index4menu.html' for '" + pkg.name + "' with version "+pkg.version);
+
+var outfile = "undefined content";
+// var outfile4menu = "undefined content";
+
+  fs.readFile('index.html', 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+      } else {
+        outfile = data.replace(/<div\s+id="version"\s+style[^<]+<\/div>/g,"<div id=\"version\" style=\"display: inline-block\">"+pkg.version+"</div>");
+        outfile = data.replace(/<div\s+id="version">[^<]+<\/div>/g,"<div id=\"version\">"+pkg.version+"</div>");
+      }
+    });
+
+setTimeout(function () {
+  b4c.save_file('index.html', outfile);
+  // b4c.save_file('index4menu.html', outfile4menu);
+},2000);
